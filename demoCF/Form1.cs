07d93b1d76527022493ce9c60ac4692a23cdb31f
tienhung36 <tienhungnguyen3622@gmail.com>
2022-05-28 08:28:20 +0700

    up test

---
+++ demoCF/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demoCF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            QLSV db = new QLSV();
+            dataGridView1.DataSource=db.SVs.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                QLSV qLSV = new QLSV();
+                foreach ( DataGridViewRow i in dataGridView1.SelectedRows )
+                {
+                    string MSSV = i.Cells["MSSV"].Value.ToString();
+                    var s = qLSV.SVs.Find(MSSV);
+                    qLSV.SVs.Remove(s);
+                    qLSV.SaveChanges();
+                }
+                dataGridView1.DataSource= qLSV.SVs.ToList();
+            }
+        }
+    }
+}
---
+++ demoCF/LSH.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoCF
+{
+    [Table("LopSH")]
+    public  class LSH
+    {
+        public LSH()
+        {
+            this.SVs = new HashSet<SV>();
+        }
+        [Key][Required]
+        public int ID_Lop { get; set; }
+        public string NameLop { get; set; }
+        public virtual ICollection<SV> SVs { get; set; }
+    }
+}
