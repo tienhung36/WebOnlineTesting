b53051324c5c0c2d02911e28b698557a9b013c3d
Tuan Hung <tuanhungg26@gmail.com>
2022-05-28 09:49:35 +0700

    HUNG CHANGE

--- OnlineTesting/OnlineTesting/Properties/Settings.Designer.cs
+++ OnlineTesting/OnlineTesting/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace OnlineTesting.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,7 @@ namespace OnlineTesting.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=BAOHUYNH;Initial Catalog=PBL3;Persist Security Info=True;User ID=sa;P" +
-            "assword=123")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=JJAYLAP;Initial Catalog=PBL3;Integrated Security=True")]
         public string ChuoiKN {
             get {
                 return ((string)(this["ChuoiKN"]));
